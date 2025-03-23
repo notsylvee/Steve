2c96cb6bc664e5fea5b37dec436d2e34240a500d
Sylvee <79902941+notsylvee@users.noreply.github.com>
2025-03-23 17:02:20 -0400

    first commit

---
+++ src/commands/commands/voiceline.js
@@ -0,0 +1,26 @@
+module.exports = {
+    data: {
+        name: "voiceline",
+        description: "Sends a random Steve voiceline",
+        "integration_types": [0, 1],
+        "contexts": [0, 1, 2]
+    },
+    async execute(interaction) {
+
+        const voicelines = [
+            "I... am Steve",
+            "The Nether",
+            "Flint and steel!",
+            "RELEASE!!!",
+            "COMIN IN HOT!!",
+            "This... is a crafting table",
+            "Chicken jockey!",
+            "Diamond Armor, full set",
+            "The stash",
+            "Theyre the Villagers!"
+        ];
+        const voiceline = voicelines[Math.floor(Math.random() * voicelines.length)];
+    
+        await interaction.reply({ content: `${voiceline}`, ephemeral: false });
+      },
+}
